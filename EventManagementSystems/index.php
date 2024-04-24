480075a4f494367c89019f1c4ce71ee5786843a2
Prashant-00-00 <sharma.prashant.8375@gmail.com>
2024-04-25 00:40:03 +0530

    Added files

---
+++ EventManagementSystems/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Event Management website</title>
+        <?php require 'utils/styles.php'; ?>
+        <?php require 'utils/scripts.php'; ?>
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?>
+        <div class="content">
+            <div class="container">
+                <div class="col-md-12">
+                    <h1>What we organize</h1>
+                </div>
+            </div>
+			
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+			
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <img src="images/meeting2.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6">
+                            <h1>Hack a thons</h1>
+                            <p>
+                                A hackathon is an event where people come together to work collaboratively on a project, 
+                                usually related to software development,
+                                with the goal of creating a functional 
+                                prototype or solution within a set amount of time.
+                            </p>
+                            <hr class="customline">
+                            <button type="button" class="btn btn-default btn-lg">
+                                View Events  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+			
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <img src="images/wedding2.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6">
+                            <h1>Wedding</h1>
+                            <p>
+                                The most important day in a couple's life. 
+                                Guaranteeing personalized solutions and flawless execution, 
+                                our venues provide the perfect location for your special day
+                            </p>
+                            <hr class="customline">
+                            <button type="button" class="btn btn-default btn-lg">
+                                View Events  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+			
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <img src="images/meeting2.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6">
+                            <h1>Concerts</h1>
+                            <p>
+                                Concerts are live performances by musicians, bands, or other performers.
+                                Attending a concert can be a great way to experience music, connect with other fans, and have a fun time.
+                            </p>
+                            <hr class="customline">
+                            <button type="button" class="btn btn-default btn-lg">
+                                View Events  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                            </button> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+			
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+            
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <img src="images/meeting2.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6">
+                            <h1>Public Gathering </h1>
+                            <p>
+                                A public gathering is an event where people come together for a common purpose, such as a festival, parade, or protest
+                            </p>
+                            <hr class="customline">
+                            <button type="button" class="btn btn-default btn-lg">
+                                View Events  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                            </button> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+			
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+            
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <img src="images/meeting2.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6">
+                            <h1>Meeting</h1>
+                            <p>
+                                From formal, to not-so-formal, our flexible event
+                                spaces can cater to your every need for meetings and conferences large or small, and our
+                                dedicated event team can assist with all aspects of your event planning.
+                            </p>
+                            <hr class="customline">
+                            <button type="button" class="btn btn-default btn-lg">
+                                View Events  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> 
+                            </button> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+            
+            <div class="container">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+        </div>
+        <?php require 'utils/footer.php'; ?>
+    </body>
+</html>
---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,39 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                
+                    <p class = "footerContent1">
+                        <strong>I</strong><span class = "small footerSubtext">nstitute of</span>
+                        <strong>C</strong><span class = "small footerSubtext">omputer</span>
+                        <strong>S</strong><span class = "small footerSubtext">cience</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                       Changigarh University
+                        &copy; Event Management Website, 2024.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    BE Computer Science Engineering (2022 - 2026)<br>
+                    Prashant Kumar(22BCS13930) ,<br> Subash Poudel(22BCS14153) ,<br> Akash Choudhary(22BCS13892) ,<br>
+                    Amit Kumar(22BCS12364) ,<br>  Muskan Tiwari(22BCS13939)
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
